eea413cc66e654be6f314e25f76af74011538c3e
TheFSilver <40010470+TheFSilver@users.noreply.github.com>
2023-05-29 04:08:14 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,17 @@
-const port = process.env.PORT || 3000
-app.listen(port, '0.0.0.0', () => {
-  console.log('server running on port ' + port)
-})
+const express = require('express');
+const { exec } = require('child_process');
+
+const app = express();
+app.listen(process.env.PORT || 3000, '0.0.0.0', function() {
+    console.log('Listening on port ' + (process.env.PORT || 3000));
+    
+    // After starting the server, start the Presearch node
+    exec('/app/presearch-node', (error, stdout, stderr) => {
+        if (error) {
+            console.error(`exec error: ${error}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+        console.error(`stderr: ${stderr}`);
+    });
+});
